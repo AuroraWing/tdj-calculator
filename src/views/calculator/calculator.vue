37ccebb2a58c3d307cf6b5a2145cf5618692fb7d
AuroraWing <2218692246@qq.com>
2022-10-30 20:07:21 +0800

    请求数据

--- src/views/calculator/calculator.vue
+++ src/views/calculator/calculator.vue
@@ -21,20 +21,45 @@ export default {
   data () {
     return {
       form: [],
-      star: 6
+      star: 6,
+      OrnamentsList: []
     }
+  },
+  created () {
+    this.axios1()
   },
   methods: {
     calculate () {
       this.getData()
-      this.check()
     },
     getData () {
       this.form = this.$refs.boardRef.form
       this.star = this.$refs.boardRef.star
       console.log(this.form)
     },
-    check () {
+    async axios1 () {
+      const axios = require('axios')
+      try {
+        const response = await axios.get('http://localhost:5050/heroData')
+        this.OrnamentsList = response.data
+        // console.log(response)
+      } catch (error) {
+        console.error(error)
+      }
+      // axios({
+      //   methods: 'get',
+      //   url: 'http://localhost:5050/heroData'
+      // })
+      //   .then(function (response) {
+      //     // 处理成功情况
+      //     that.OrnamentsList = response.data
+      //     console.log(response.data)
+      //   })
+      //   .catch((error) => {
+      //     console.log(error)
+      //   })
+      // this.OrnamentsList =
+      console.log(this.OrnamentsList)
     }
   }
 }
